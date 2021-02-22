e1162aa8d88706291ba13d5163eb2d984a953a61
jjpokey <jasonelliott99@yahoo.com>
2021-02-21 18:07:50 -0800

    BinaryPrinterFinished

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,16 +6,15 @@ public class BinaryPrinter {
 	 * Do not use the Integer.toBinaryString method!
 	 * Use the main method to test your methods.
 	 */
-
+int count = 0;
 	
-	public void printByteBinary(byte b) { //not done
+	public void printByteBinary(byte b) { 
 		// We first want to print the bit in the one's place
 		 
 		String converted = (byte) (b & 1) + "";
-		System.out.println(converted);
 		String convertFinish = "";
 		
-		for(int i = 0; i < 8; i++) { // (this is the last step, not done correctly should ask)
+		for(int i = 0; i < 8; i++) { 
 			
 			// Shift b seven bits to the right
 			
@@ -30,38 +29,74 @@ public class BinaryPrinter {
 		b = (byte) (b >> 1);
 		
 		}
+		if(count == 0) {
+			System.out.println("\n Byte:");
+		}
+		else if(count == 1) {
+			System.out.println("\n Short:");
+		}
+		else if(count == 2) {
+			System.out.println("\n Int:");
+		}
+		else if(count == 3) {
+			System.out.println("\n Long:");
+		}
 		System.out.println(convertFinish);
 	}
 	
 	public void printShortBinary(short s) {
+		if(!(count >= 1)) {
+		count = 1;
+		}
 		// Create 2 byte variables
 		byte a;
 		byte b;
 		// Use bit shifting and masking (&) to save the first
 		// 8 bits of s in one byte, and the second 8 bits of
 		// s in the other byte
+
+		a = (byte) (s);
+		b = (byte) (s >> 8);
+		
 		
 		// Call printByteBinary twice using the two bytes
 		// Make sure they are in the correct order
+			printByteBinary(a);
+			printByteBinary(b);
+		
 	}
 	
 	public void printIntBinary(int i) {
+		if(!(count >= 2)) {
+			count = 2;
+			}
 		// Create 2 short variables
 		short a;
 		short b;
 		// Use bit shifting and masking (&) to save the first
-		// 16 bits of i in one short, and the second 16 bits of
 		// i in the other short
-		
+		a = (short) (i);
+		b = (short) (i >> 16);
 		// Call printShortBinary twice using the two short variables
 		// Make sure they are in the correct order
+		printShortBinary(a);
+		printShortBinary(b);
 	}
 	
 	public void printLongBinary(long l) {
+		if(!(count >= 3)) {
+			count = 3;
+			}
 		// Use the same method as before to complete this method
-		long a;
-		long b;
+		int a;
+		int b;
 		//continue
+		a = (int) (l);
+		b = (int) (l >> 32);
+		// Call printShortBinary twice using the two short variables
+		// Make sure they are in the correct order
+		printIntBinary(a);
+		printIntBinary(b);
 	}
 	
 	public static String convertToBinary(int input) { //NOT USING THIS
@@ -85,11 +120,13 @@ public class BinaryPrinter {
 		
 	}
 	
-	
 	public static void main(String[] args) {
 		// Test your methods here
 		BinaryPrinter br = new BinaryPrinter();
 		
-		br.printByteBinary((byte) 14); //not sure about input
+		br.printByteBinary((byte) 14); 
+		br.printShortBinary((short) 300);
+		br.printIntBinary(33000);
+		br.printLongBinary((long) 3000000000l);
 	}
 }
